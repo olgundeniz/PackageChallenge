a27b82540069a25641f7798a4d73c23e354b4b07
AD001\z003zu0r <deniz.olgun@siemens.com>
2021-07-12 16:56:19 +0300

    Test assertion is expanded

--- PackageChallenge/Tests/UnitTests.cs
+++ PackageChallenge/Tests/UnitTests.cs
@@ -20,21 +20,38 @@ namespace Tests
             var expectedItemLists = new List<List<Item>>();
             var expectedListByLine = new List<Item>();
 
+            //arrange
             var newItem1 = new Item() { Index=1, Weight= 53.38m , Cost=45 };
             var newItem2 = new Item() { Index = 2, Weight = 88.62m, Cost = 98 };
-            var newItem3 = new Item() { Index = 2, Weight = 15.3m, Cost = 34 };
+            var newItem3 = new Item() { Index = 3, Weight = 78.48m, Cost = 3 };
+            var newItem4 = new Item() { Index = 4, Weight = 72.30m, Cost = 76 };
+            var newItem5 = new Item() { Index = 5, Weight =30.18m, Cost = 9 };
+            var newItem6 = new Item() { Index = 6, Weight = 46.34m, Cost = 48 };
 
-            expectedItemLists.Add(new List<Item>() { newItem1, newItem2 });
-            expectedItemLists.Add(new List<Item>() { newItem3 });
+            var newItem7 = new Item() { Index = 1, Weight = 15.3m, Cost = 34 };
+
+            expectedItemLists.Add(new List<Item>() { newItem1, newItem2, newItem3, newItem4, newItem5, newItem6 });
+            expectedItemLists.Add(new List<Item>() { newItem7 });
 
             string inputFilePath = @".\example_input";
 
+            //act
             itemLists = Utilities.ReadInput(inputFilePath, out maxWeights);
 
+            //assert max weights
             Assert.AreEqual(expectedMaxWeights[0], maxWeights[0]);
             Assert.AreEqual(expectedMaxWeights[1], maxWeights[1]);
             Assert.AreEqual(expectedMaxWeights[2], maxWeights[2]);
             Assert.AreEqual(expectedMaxWeights[3], maxWeights[3]);
+
+            //assert items in the test file
+            for (int i = 0; i < expectedItemLists.Count; i++)
+            {
+                for (int j = 0; j < expectedItemLists[i].Count; j++)
+                {
+                    Assert.AreEqual(expectedItemLists[i][j], itemLists[i][j]);
+                }
+            }
         }
     }
 }
